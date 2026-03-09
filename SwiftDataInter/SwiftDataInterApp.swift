adae00f57383cf1860244d5c35d68077205c28dd
wannabemountainking <kdy0096@gmail.com>
2026-03-09 10:58:47 +0900

    Initial Commit

---
+++ SwiftDataInter/SwiftDataInterApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftDataInterApp.swift
+//  SwiftDataInter
+//
+//  Created by YoonieMac on 3/9/26.
+//
+
+import SwiftUI
+
+@main
+struct SwiftDataInterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
